003111cb623ef2dac1fb9e1a1f9e80d3b4b196d6
Jamie <phillipsja97@gmail.com>
2020-03-26 11:23:34 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="btn btn-danger">Hello</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
